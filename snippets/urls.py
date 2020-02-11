d7d8273ba6289d54805731c38659ed86053f7eb9
JEONGSUJI <wtw9611@gmail.com>
2020-02-11 18:22:43 +0900

    Class-based view(APIView)기반으로 snippet list, detail 구현

--- snippets/urls.py
+++ snippets/urls.py
@@ -1,9 +1,14 @@
 from django.urls import path
 
-from . import views
+from . import apis
 
 app_name = 'snippets'
+
 urlpatterns = [
-    path('snippets/', views.snippet_list),
-    path('snippets/<int:pk>/', views.snippet_detail),
+    # path('snippets/', views.snippet_list),
+    # path('snippets/<int:pk>/', views.snippet_detail),
+
+    # Class-based view를 사용하는 경우, as_view()함수를 호출
+    path('snippets/', apis.SnippetListCreateAPIView.as_view()),
+    path('snippets/<int:pk>/', apis.SnippetRetrieveUpdateDestroyAPIView.as_view()),
 ]
