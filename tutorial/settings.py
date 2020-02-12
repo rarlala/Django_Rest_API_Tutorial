bc1a5afc6e626f598a00f86722bfa6a6133455d0
JEONGSUJI <wtw9611@gmail.com>
2020-02-12 15:06:46 +0900

    members app추가

--- tutorial/settings.py
+++ tutorial/settings.py
@@ -27,10 +27,12 @@ DEBUG = True
 
 ALLOWED_HOSTS = []
 
+AUTH_USER_MODEL = 'members.User'
 
 # Application definition
 
 INSTALLED_APPS = [
+    'members.apps.MembersConfig',
     'snippets.apps.SnippetsConfig',
     
     'django.contrib.admin',
